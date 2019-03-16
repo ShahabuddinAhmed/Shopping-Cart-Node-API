bdb156cf75e83ba289a6c90c562c2e6a2a0b66a7
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-16 17:42:47 +0600

    user and admin route is added

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const userRoutes = require('./api/routes/user');
-const adminRoutes = require('./api/routes/admin');
-const postRoutes = require('./api/routes/post');
-const CommentRoutes = require('./api/routes/comment');
-const replaytRoutes = require('./api/routes/replay');
-const aboutWriterRoutes = require('./api/routes/about-writer');
-const catagoryRoutes = require('./api/routes/catagory');
+const userRoutes = require('./api/routes/user/user');
+const userShoppingListRoutes = require('./api/routes/user/shopping-list');
+const adminRoutes = require('./api/routes/admin/admin');
+const categoryRoutes = require('./api/routes/admin/category');
+const productRoutes = require('./api/routes/admin/product');
+const adminShoppingListRoutes = require('./api/routes/admin/shopping-list');
 
-mongoose.connect('mongodb://localhost:27017/PersonalBlog', {
+mongoose.connect('mongodb://localhost:27017/ShoppingList', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
@@ -26,12 +25,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/user', userRoutes);
+app.use('/usershoppinglist', userShoppingListRoutes);
 app.use('/admin', adminRoutes);
-app.use('/post', postRoutes);
-app.use('/comment', CommentRoutes);
-app.use('/replay', replaytRoutes);
-app.use('/about', aboutWriterRoutes);
-app.use('/catagory', catagoryRoutes);
+app.use('/category', categoryRoutes);
+app.use('/product', productRoutes);
+app.use('/adminshoppinglist', adminShoppingListRoutes);
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
