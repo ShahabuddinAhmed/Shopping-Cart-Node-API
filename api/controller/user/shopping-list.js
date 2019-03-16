0c5e56ab450ff84a60b0eaeb2443e1474053b0cd
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2019-03-16 17:43:25 +0600

    user shopping-list controller is added

---
+++ api/controller/user/shopping-list.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const ShoppingLists = require('../../models/user/shopping-list');
+
+exports.getAllShoppingList = (req, res, next) => {
+    ShoppingLists.find()
+    .exec()
+    .then(docs => {
+        if(docs.length >=1) {
+            console.log("From database", docs);
+            res.status(200).json(docs);
+        } else {
+            res.status(200).json({
+                message: "Shopping Lists document is empty."
+            });
+        }
+    })
+    .catch(err => {
+        res.status(200).json({
+            message: err
+        });
+    });
+}
